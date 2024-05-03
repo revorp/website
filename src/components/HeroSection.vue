e1f7b026c5a9fd36f98fe5812fe841d205cb30bf
alerunza <alessandrorunza@icloud.com>
2024-05-03 10:42:29 +0200

    add herosection

---
+++ src/components/HeroSection.vue
@@ -0,0 +1,40 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <section class="relative h-screen flex items-center justify-center">
+    <div
+      class="absolute inset-0 m-auto h-80 max-w-lg bg-gradient-to-tr from-indigo-400 via-teal-900 to-[#C084FC] blur-[118px]">
+    </div>
+    <div class="relative mx-auto max-w-screen-xl gap-12 px-4 py-28 text-slate-400 md:px-8">
+      <div class="mx-auto max-w-4xl text-center">
+        <div class="group relative inline-block items-center">
+          <span
+            class="flex size-6 cursor-pointer items-center justify-center rounded-full bg-slate-900 text-sm font-semibold text-white">?</span>
+          <span role="tooltip"
+            class="pointer-events-none absolute z-50 inline-flex items-center justify-center rounded-md px-3 py-1 italic opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 bg-slate-900 text-white min-h-6 text-xs left-1/2 -translate-x-1/2 after:absolute after:block after:size-0 after:border-8 after:border-transparent after:border-t-slate-900 after:-bottom-[0.95rem] after:left-1/2 after:-translate-x-1/2 top-[-2.25rem]">
+            dream</span>
+        </div>
+        <div class="space-y-5">
+          <h2 class="mx-auto text-4xl font-extrabold text-slate-50 md:text-5xl">revo
+            <br> <span class="bg-gradient-to-r from-blue-500 to-[#E114E5] bg-clip-text text-transparent">coming
+              soon</span>
+          </h2>
+          <p class="mx-auto max-w-2xl">a new way to play roleplay in Italy, join us now and be part of the <span
+              class="font-bold underline italic">dream</span>.</p>
+          <div class="items-center justify-center gap-x-3 space-y-3 text-sm sm:flex sm:space-y-0">
+            <a class="inline-flex justify-center items-center gap-x-3 text-center bg-gradient-to-tl from-blue-600 to-violet-600 hover:from-violet-600 hover:to-blue-600 border border-transparent text-white text-sm font-medium rounded-full py-3 px-4 dark:focus:ring-offset-gray-800"
+              href="#">
+              <svg class="flex-shrink-0 size-4" xmlns="http://www.w3.org/2000/svg" width="16" height="16"
+                fill="currentColor" viewBox="0 0 16 16">
+                <path
+                  d="M13.545 2.907a13.2 13.2 0 0 0-3.257-1.011.05.05 0 0 0-.052.025c-.141.25-.297.577-.406.833a12.2 12.2 0 0 0-3.658 0 8 8 0 0 0-.412-.833.05.05 0 0 0-.052-.025c-1.125.194-2.22.534-3.257 1.011a.04.04 0 0 0-.021.018C.356 6.024-.213 9.047.066 12.032q.003.022.021.037a13.3 13.3 0 0 0 3.995 2.02.05.05 0 0 0 .056-.019q.463-.63.818-1.329a.05.05 0 0 0-.01-.059l-.018-.011a9 9 0 0 1-1.248-.595.05.05 0 0 1-.02-.066l.015-.019q.127-.095.248-.195a.05.05 0 0 1 .051-.007c2.619 1.196 5.454 1.196 8.041 0a.05.05 0 0 1 .053.007q.121.1.248.195a.05.05 0 0 1-.004.085 8 8 0 0 1-1.249.594.05.05 0 0 0-.03.03.05.05 0 0 0 .003.041c.24.465.515.909.817 1.329a.05.05 0 0 0 .056.019 13.2 13.2 0 0 0 4.001-2.02.05.05 0 0 0 .021-.037c.334-3.451-.559-6.449-2.366-9.106a.03.03 0 0 0-.02-.019m-8.198 7.307c-.789 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.45.73 1.438 1.613 0 .888-.637 1.612-1.438 1.612m5.316 0c-.788 0-1.438-.724-1.438-1.612s.637-1.613 1.438-1.613c.807 0 1.451.73 1.438 1.613 0 .888-.631 1.612-1.438 1.612" />
+              </svg>
+              join now!
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
