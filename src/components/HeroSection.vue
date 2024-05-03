fd779269bf28603aff20a7d1ae7a442bb8b22f01
runzac <91915526+alerunza@users.noreply.github.com>
2024-05-03 19:46:58 +0200

    add transitions

--- src/components/HeroSection.vue
+++ src/components/HeroSection.vue
@@ -8,20 +8,20 @@
     </div>
     <div class="relative mx-auto max-w-screen-xl gap-12 px-4 py-28 text-slate-400 md:px-8">
       <div class="mx-auto max-w-4xl text-center">
-        <div class="group relative inline-block items-center">
+        <!-- <div class="group relative inline-block items-center animate-slidein">
           <span
             class="flex size-6 cursor-pointer items-center justify-center rounded-full bg-slate-900 text-sm font-semibold text-white">?</span>
           <span role="tooltip"
             class="pointer-events-none absolute z-50 inline-flex items-center justify-center rounded-md px-3 py-1 italic opacity-0 transition-opacity duration-300 ease-in-out group-hover:opacity-100 bg-slate-900 text-white min-h-6 text-xs left-1/2 -translate-x-1/2 after:absolute after:block after:size-0 after:border-8 after:border-transparent after:border-t-slate-900 after:-bottom-[0.95rem] after:left-1/2 after:-translate-x-1/2 top-[-2.25rem]">
             dream</span>
-        </div>
-        <div class="space-y-5">
+        </div> -->
+        <div class="space-y-5 animate-slidein">
           <h2 class="mx-auto text-4xl font-extrabold text-slate-50 md:text-5xl">revo
             <br> <span class="bg-gradient-to-r from-blue-500 to-[#E114E5] bg-clip-text text-transparent">coming
               soon</span>
           </h2>
           <p class="mx-auto max-w-2xl">a new way to roleplay in Italy, <a
-              class="underline text-lg font-bold hover:text-blue-500 hover:no-underline"
+              class="underline font-bold hover:text-blue-500 hover:no-underline transition duration-700 ease-in-out transform hover:text-lg"
               href="https://discord.gg/GBpzzvsVZ8">join us</a>
             now and
             be part of the
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,23 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        slidein: {
+          from: {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          to: {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        slidein: "slidein 2s ease 300ms",
+      },
+    },
   },
   plugins: [],
 }
